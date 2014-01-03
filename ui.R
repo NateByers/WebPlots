11cb2814253c585c0a5140f58743789eefacc410
NateByers <nbyers@idem.in.gov>
2014-01-03 15:36:27 -0500

    Deleted git text from ui script.

--- ui.R
+++ ui.R
@@ -19,11 +19,8 @@ shinyUI(pageWithSidebar(
     tabsetPanel(
       tabPanel("Time: Morris", showOutput("timeMorris", "morris")),
       tabPanel("Time: High", showOutput("timeHigh", "Highcharts")),
-<<<<<<< HEAD
       #tabPanel("Time: Rickshaw", showOutput("timeRickshaw", "rickshaw")),
-=======
       tabPanel("Time: Rickshaw", showOutput("timeRickshaw", "rickshaw")),
->>>>>>> 806f87961060edaaf24e29922cbc642772c95dbe
       tabPanel("Time: NVD3", showOutput("timeNvd3", "nvd3")),
       tabPanel("Box: High", showOutput("boxHigh", "Highcharts"))
       
